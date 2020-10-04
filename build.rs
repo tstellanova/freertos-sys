f831c4bd49f38648bb2808e1119e6d67c12edacc
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-10-03 19:04:24 -0700

    provide for better debugging of make failures

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 fn main() {
     let work_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
-    eprintln!("out_dir: {}",out_dir);
+    println!("out_dir: {}",out_dir);
 
     #[cfg(not(feature="family-selected"))]
         let platform_subdir = "cmsis_freertos_stm32f4";
@@ -23,7 +23,7 @@ fn main() {
 
 
     let platform_dir = format!("{}/platforms/{}", work_dir, platform_subdir);
-    eprintln!("platform_dir: {}",platform_dir);
+    println!("platform_dir: {}",platform_dir);
 
     // force a rebuild on change
     println!("cargo:rerun-if-changed={}",platform_dir);
@@ -37,13 +37,17 @@ fn main() {
     let lib_out_dir = format!("{}/freertos_build",out_dir);
     let build_dir = format!("BUILD_DIR={}",lib_out_dir);
 
-    Command::new("make")
+    let cmd_output = Command::new("make")
         .current_dir(platform_dir.clone())
         .arg(debug_flag)
         .arg(opt_flag)
         .arg(build_dir)
-        .output()
-         .expect("make failed ");
+        .output().expect("could not run command");
+
+    if !cmd_output.status.success() {
+        println!("\n==== make result: {} === \n {:?} \n============", cmd_output.status, cmd_output);
+        panic!("freertos platform make failed");
+    }
 
     // static library should now be in the build directory of the specific platform
     // println!("cargo:rustc-link-search={}/build/", platform_dir.clone());
